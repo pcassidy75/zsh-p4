c1d74a0794a929ba9e3b1410a38a4c397ac091c6
Paul Cassidy <pcassidy@synopsys.com>
2022-01-01 17:21:22 +0000

    cosmetic updates

--- zsh-p4.plugin.zsh
+++ zsh-p4.plugin.zsh
@@ -1,22 +1,21 @@
 #!/bin/zsh
 # Attempt at making a prompt which automatically gives p4 branch, status etc.
 # Idea:
-#   Use p4 info to figure out if we are in a p4 client
 #   Get 'branch' from p4 client -o: By convention the branch is the third 'level' //<depot>/<product>/<branch>
 #   Get status from p4 opened
-#   Get Remote from p4 sync -n
+#   Todo: Get Remote from p4 sync -N | grep 0/0/0
 #   Use $ZSH_THEME_GIT* settings for a consistent look and feel
 function p4_prompt_info() {
 
 # Get 'Branch' info (fifth field from //<depot>/<product>/<branch>)
-  local ref
+  local branch
   # weird redirects need to preserve exit status from p4 client (better way?)
-  ref=$(grep '//' < <(p4 client -o 2> /dev/null) > >(tail -n 1 | cut -d / -f 5)) || return 0
+  branch=$(grep '//' < <(p4 client -o 2> /dev/null) > >(tail -n 1 | cut -d / -f 5)) || return 0
 
   # Todo: Integrate p4 sync info into upstream
   local upstream
 
-  echo "${ZSH_THEME_GIT_PROMPT_PREFIX}${ref:gs/%/%%}${upstream:gs/%/%%}$(parse_p4_dirty)${ZSH_THEME_GIT_PROMPT_SUFFIX}"
+  echo "${ZSH_THEME_GIT_PROMPT_PREFIX}${branch:gs/%/%%}${upstream:gs/%/%%}$(parse_p4_dirty)${ZSH_THEME_GIT_PROMPT_SUFFIX}"
 }
 
 function parse_p4_dirty() {
